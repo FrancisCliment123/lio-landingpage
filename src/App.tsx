b52f52ec0b90ef03198e090a9f998594ea9afd6b
FrancisCliment123 <franciscojavier.climent@alumni.esade.edu>
2026-02-12 12:44:02 +0100

    fix: automatic scroll to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Privacy from './Privacy';
 import Terms from './Terms';
+import ScrollToTop from './ScrollToTop';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/privacy" element={<Privacy />} />
-      <Route path="/terms" element={<Terms />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
+    </>
   );
 }
